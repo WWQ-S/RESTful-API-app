7864e518329816bb8047518e32f9a18a148621db
Viktor Zhilkin <007viktor25@gmail.com>
2023-12-05 15:49:40 +0600

    Add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ColumnModule } from './column/column.module';
+import { CardModule } from './card/card.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ColumnModule, CardModule, CommentModule],
   controllers: [AppController],
   providers: [AppService],
 })
